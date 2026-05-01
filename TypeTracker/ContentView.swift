3fb50fb7747d29b5ccfacaad5802e424d0228d37
himani <himani44prasad@gmail.com>
2026-05-01 19:26:24 +0530

    Initial Commit

---
+++ TypeTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TypeTracker
+//
+//  Created by Himani on 01/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TypeTracker/TypeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeTrackerApp.swift
+//  TypeTracker
+//
+//  Created by Himani on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TypeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
